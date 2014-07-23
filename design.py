bb7021603350428f4c4b95e48017009e788076ff
Michael Liu <michael@zamplus.com>
2014-07-23 18:45:06 +0800

    add design file

---
+++ design.py
@@ -0,0 +1,47 @@
+
+class Corpus:
+    doc_list = []
+
+class Document:
+    word_list = []
+
+class Word:
+    topic_list = []
+
+class Model:
+    topic_list = []
+
+class Topic:
+    word_list = []
+
+class SemiLDA:
+
+    def parse_args(self, args):
+        pass
+
+    def init_model(self):
+        pass
+
+    def init_corpus(self): 
+        pass
+
+    def train(self):
+        pass
+
+    def infer(self):
+        pass
+
+    def sample_corpus(self):
+        pass
+
+    def sample_doc(self):
+        pass
+
+    def sample_word(self):
+        pass
+
+    def stat_corpus(self):
+        pass
+
+    def loglikelihood(self):
+        pass
