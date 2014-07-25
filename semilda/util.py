077abbd550503bb10eb53a9150477f851ee95977
Zuotao Liu <fancyspeed@163.com>
2014-07-25 08:54:13 +0800

    add util

--- semilda/util.py
+++ semilda/util.py
@@ -1,34 +1,42 @@
 #!/usr/bin/env python
 
 import sys
-import getopt
+#import getopt
+import argparse
 
+class ArgUtil(object):
 
-def Usage():
-    print 'usage:'
-    print '-h,--help: print help message.'
-    print '-o,--output: output path' 
-    print '--k: num of topics'
-    print '-a,--alpha: alpha'
-    print '-b,--beta: beta'
+    def __init__(self):
+        self.base_parser = argparse.ArgumentParser(add_help=False)
+        self.base_parser.add_argument('-model', help='model file path')
+        self.base_parser.add_argument('-alpha', help='alpha')
+        self.base_parser.add_argument('-beta', help='beta')
+        self.base_parser.add_argument('-burnin', dest='burn_in', type=int, default=20, help='burn in iteration')
 
-def main(argv):
-    try:
-        opts, args = getopt.getopt(argv[1:], 
-    except getopt.GetoptError, err:
-        print repr(err)
-        Usage()
-        exit.exit(10
-    for o, a in opts:
-        if o in ('-h', '--help'):
-            print Usage()
-            exit(1)
-        if o in ('-o', '--output'):
-            print a
-            exit(1)
+        self.train_parser = argparse.ArgumentParser(parents=[self.base_parser],
+                                         description='Semi-supervised LDA.',
+                                         epilog='if inference, please use lda_infer.py')
+        self.train_parser.add_argument('-train', help='training data')
+        self.train_parser.add_argument('-rule', nargs='?', help='rule data (optional)')
+        self.train_parser.add_argument('-k', help='total number of topics')
+        self.train_parser.add_argument('-iter', dest='max_iter', type=int, default=50, help='total iteration')
+
+        self.infer_parser = argparse.ArgumentParser(parents=[self.base_parser],
+                                         description='Semi-supervised LDA.',
+                                         epilog='if training, please use lda_train.py')
+        self.infer_parser.add_argument('-test', help='testing data')
+        self.infer_parser.add_argument('-output', help='output path of inference')
+
+    def parse_train_args(self):
+        args = self.train_parser.parse_args()
+        print args.train
+
+    def parse_infer_args(self):
+        args = self.infer_parser.parse_args()
+        print args.test
 
 if __name__ == '__main__':
-    main(sys.argv)
+    util = ArgUtil()
+    util.parse_train_args()
 
-    
-    
+
